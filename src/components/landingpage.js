ffef5187e653afc990f2bf789a63e281e0a69a26
AidanFray <afray@hotmail.co.uk>
2018-06-04 19:01:12 +0000

    Changes; - Added code logos onto cards

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -23,6 +23,7 @@ class Landing extends Component {
                 url="https://s15.postimg.cc/ghwy5traz/diss_proj.png"
                 card_text="Compromised and Degraded Network Simulation - The aim of the dissertation project was to create a custom simulated network that can demonstrate and visualise network degradation and common DoS attacks.
                 It uses the NetFilter section of the Linux Kernel to filter and minipulate network packets."
+                languages="Python,Linux"
                 github_url="https://github.com/AidanFray/Dissertation-Project"/>
 
               <ProjectCard
@@ -30,6 +31,7 @@ class Landing extends Component {
                 text_color = '#000'
                 url="https://raw.githubusercontent.com/AidanFray/gh-pages-portfolio/master/src/project_images/scale_logo.png"
                 card_text="An application that is designed to randomly provide scales for practising musicians. This app is produced using Xamarin to proivde a cross platform application for iOS and Android."
+                languages="C#,Android,iOS"
                 github_url="https://github.com/AidanFray/ScaleGenerator-iOS_Andriod"/>
 
               <ProjectCard
@@ -37,6 +39,7 @@ class Landing extends Component {
                 text_color = '#fff'
                 url="https://raw.githubusercontent.com/AidanFray/gh-pages-portfolio/master/src/project_images/3d_sim.png"
                 card_text="A university graphics and simulation project where the aim was to simulate various numbers of spheres falling through a column."
+                languages="C#"
                 github_url="https://github.com/AidanFray/Simulation-and-3D-Graphics"/>
 
               <ProjectCard
@@ -44,6 +47,7 @@ class Landing extends Component {
                 text_color = '#c024fe'
                 url="https://raw.githubusercontent.com/AidanFray/gh-pages-portfolio/master/src/project_images/interactive_map.png"
                 card_text="A Javascript interactive map of Hull to celebrate the City of Culture."
+                languages="JS,HTML5"
                 github_url="https://github.com/AidanFray/Interactive_Map"/>
 
               <ProjectCard
@@ -51,6 +55,7 @@ class Landing extends Component {
                 text_color = '#000'
                 url="https://raw.githubusercontent.com/AidanFray/gh-pages-portfolio/master/src/project_images/mobile_logo.png"
                 card_text="Sliding puzzle coursework submitted for a university mobile devices module. The goal was to create a small sliding puzzle game with dynamically download puzzles."
+                languages="Java,Android"
                 github_url="https://github.com/AidanFray/Mobile_Devices_And_Applications"/>
 
               <ProjectCard
@@ -58,6 +63,7 @@ class Landing extends Component {
                 text_color = '#fff'
                 url="https://raw.githubusercontent.com/AidanFray/gh-pages-portfolio/master/src/project_images/hull_logo.png"
                 card_text="Program that web scrapes the https://timetable.hull.ac.uk/ website to sync your timetable with outlook"
+                languages="C#"
                 github_url="https://github.com/AidanFray/Hull_Timetable_To_Outlook"/>
 
             </div>
--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -3,25 +3,73 @@ import {Card, CardTitle, CardText, CardActions, Button, CardMenu} from 'react-md
 
 
 class ProjectCard extends Component {
+
+
+  languages =
+  [
+    ["C++", "devicon-cplusplus-plain"],
+    ["Java", "devicon-java-plain"],
+    ["Python", "devicon-python-plain"],
+    ["C#", "devicon-csharp-plain"],
+    ["JS", "devicon-javascript-plain"],
+    ["HTML5", "devicon-html5-plain"],
+    ["Android", "devicon-android-plain"],
+    ["Linux", "devicon-linux-plain"],
+    ["iOS", "devicon-apple-original"],
+    ["Windows", "devicon-windows8-original"]
+  ]
+
+  langauges_logos = []
+
+  languages_proc(text) {
+    var sentence = text;
+
+    if (text != undefined) {
+      var parts = sentence.split(",");
+
+      for (var i = 0; i < parts.length; i++) {
+        var lang = parts[i];
+
+        for (var x = 0; x < this.languages.length; x++) {
+          if (this.languages[x][0].trim() == lang) {
+            this.langauges_logos.push(<i style={{fontSize: "25px", padding: "2px"}} class={this.languages[x][1]}></i>);
+          }
+        }
+      }
+
+      console.log(parts)
+    }
+  }
+
+
   render() {
+    {this.languages_proc(this.props.languages)}
     return (
       <div className="project-item">
         <Card shadow={0} style={{minWidth: '250px', margin: 'auto'}}>
+
           <CardTitle style={{alignItems: 'flex-start', color: this.props.text_color, height: '200px', width: '330px', background: 'url('+ this.props.url +') center / cover'}}>
-          <div className="card-title">
-            {this.props.title}
-          </div>
-        </CardTitle>
-        <CardText>
-          <div className="card-text">
-            {this.props.card_text}
-          </div>
-        </CardText>
-        <CardActions border>
-          <Button colored href={this.props.github_url}>GitHub</Button>
-        </CardActions>
-        <CardMenu style={{color: this.props.text_color}}>
-        </CardMenu>
+            <div className="card-title">
+              {this.props.title}
+            </div>
+          </CardTitle>
+
+          <CardText>
+            <div className="card-text">
+              {this.props.card_text}
+            </div>
+          </CardText>
+
+          <CardActions border>
+              {this.langauges_logos}
+          </CardActions>
+
+          <CardActions border>
+            <Button colored href={this.props.github_url}>GitHub</Button>
+          </CardActions>
+
+          <CardMenu style={{color: this.props.text_color}}>
+          </CardMenu>
       </Card>
     </div>
   )
