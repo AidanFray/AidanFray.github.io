34a63337887fdc600e1a572088cefa017b838dbc
Aidan Fray <afray@protonmail.com>
2019-01-11 14:54:58 +0000

    Updates

---
+++ precache-manifest.7abd47abf7ce1d45bd433ca087da4a05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "335adec3a2897bbec884",
+    "url": "/Portfolio/static/css/main.931f22e3.chunk.css"
+  },
+  {
+    "revision": "335adec3a2897bbec884",
+    "url": "/Portfolio/static/js/main.335adec3.chunk.js"
+  },
+  {
+    "revision": "e671f761185770c0fc09",
+    "url": "/Portfolio/static/css/1.306655ed.chunk.css"
+  },
+  {
+    "revision": "e671f761185770c0fc09",
+    "url": "/Portfolio/static/js/1.e671f761.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "7263c83670eb8d6c398634940db7a660",
+    "url": "/Portfolio/index.html"
+  }
+];
